c563b272637dcd609d524c2bd807b0d3e94cfdd1
= <=>
2024-11-22 22:57:30 +0530

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  env: {
+    DATABASE_URL: process.env.DATABASE_URL,
+  },
+};
+
+export default nextConfig;
